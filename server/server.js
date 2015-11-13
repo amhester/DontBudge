5e183ffd7f853490e0a77e22c7fb7b673fb3eed3
amhester <amhester@anderson.edu>
2015-11-13 18:53:31 -0500

    some initial stuff
    run npm install in a terminal in the project directory

---
+++ server/server.js
@@ -0,0 +1,35 @@
+"use strict";
+
+/* --------------- Require Stuff ---------------------- */
+var config = require('./app.config.json');
+var bunyan = require('bunyan');
+var restify = require('restify');
+
+/* --------------- Other Global Initialization -------- */
+var log = bunyan.createLogger('com.DontBudge.server.logger');
+
+var server = restify.createServer({
+    name: config.name,
+    version: config.version
+});
+
+/* --------------- listening to server events --------- */
+server.on('after', function (req, res, route, error) {
+
+});
+
+/* --------------- Register Server Middleware --------- */
+server.use(restify.acceptParser(server.acceptable));
+server.use(restify.dateParser());
+server.use(restify.queryParser());
+server.use(restify.gzipResponse());
+server.use(restify.bodyParser());
+server.use(restify.CORS());
+
+/* --------------- Register Routes Here --------------- */
+
+
+/* --------------- Start the server listening --------- */
+server.listen(config.port, config.host, function () {
+    log.info(config.name + ' is now running, listening on ' + server.url);
+});
