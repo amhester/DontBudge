f942345462e970ad0a1cc1d275f3c80ba6f0e7cf
amhester <amhester@anderson.edu>
2015-11-13 20:58:45 -0500

    more stuff

---
+++ server/routes/routes.js
@@ -0,0 +1,22 @@
+"use strict";
+
+var config = require('./../app.config.json');
+var bunyan = require('bunyan');
+var yodleeService = require('./../yodleeService.js');
+
+module.exports = {};
+module.exports.register = function (server) {
+    //example route
+    server.get('/', function (req, res, next) {
+        yodleeService.doSomething('something', function (err, results) {
+            if(err) {
+                res.send(500, "Error in yodleeService");
+            } else {
+                //transform results here for response
+                res.send(200, results);
+            }
+        });
+    });
+
+    //other routes can be applied below here following the same pattern as above
+};
---
+++ server/services/yodleeService.js
@@ -0,0 +1,17 @@
+"use strict";
+
+var config = require('./../app.config.json');
+var bunyan = require('bunyan');
+var request = require('request');
+
+class YodleeService {
+    constructor () {
+
+    }
+
+    doSomething (something, callback) {
+        request('some-http-route-here' + something, function (err, res, body) {
+            callback(err, res, body);
+        });
+    }
+}
