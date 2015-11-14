0882c32264dae3583b4698f23d07efc4898e4db2
Neal Hamilton <nealhamilton92@gmail.com>
2015-11-13 22:12:41 -0500

    Update routes.js
    fixing merge conflicts

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -3,10 +3,7 @@
 var config = require('./../app.config.json');
 var bunyan = require('bunyan');
 var YodleeService = require('./../services/yodleeService.js');
-<<<<<<< HEAD
 var yodleeService = new YodleeService();
-=======
->>>>>>> origin/master
 
 module.exports = {};
 module.exports.register = function (server) {
