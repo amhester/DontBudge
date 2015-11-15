7ffce42651797f9190540853c0fe78d08ca6e87a
Neal Hamilton <nealhamilton92@gmail.com>
2015-11-15 00:09:07 -0500

    getUserResults implemented but NOT tested yet. most of the yodlee service needs tested once redis is up and running.

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -48,5 +48,55 @@ module.exports.register = function (server) {
 
     });
 
+    server.get('/userLogout', function(req, res, next) {
+        yodleeService.userLogout(cobToken, userToken, function(err, result) {
+            if (err) {
+                res.send(500, "Error userLogout.");
+            }
+            else {
+                res.send(200, result);
+            }
+        });
+    });
+
+    server.get('/validUser', function(req, res, next) {
+       yodleeService.isValidUser(cobToken, config.apis.yodlee.testUser1.username, function(err, result) {
+           if (err) {
+               res.send(500, "Error validUser");
+           }
+           else {
+               res.send(200, result);
+           }
+       });
+    });
+
+    server.get('/userResults', function(req, res, next) {
+       yodleeService.getUserResults(cobToken, userToken, {
+           containerType: req.params.containerType,
+           higherFetchLimit: req.params.higherFetchLimit,
+           lowerFetchLimit: req.params.lowerFetchLimit,
+           resultRange: {
+               endNumber: req.params.endNumber,
+               startNumber: req.params.endNumber
+           },
+           searchClients: {
+               clientId: req.params.clientId,
+               clientName: req.params.clientName
+           },
+           userInput: req.params.userInput,
+           ignoreUserInput: req.params.ignoreUserInput,
+           searchFilter: {
+               currencyCode: req.params.currencyCode,
+               postDateRange: {
+                   fromDate: req.params.fromDate,
+                   toDate: req.params.toDate
+               },
+               transactionSplitType: req.params.transactionSplitType,
+               itemAccountId: {
+                   identifier: req.params.identifier
+               }
+           }
+       });
+    });
     //other routes can be applied below here following the same pattern as above
 };
--- server/services/yodleeService.js
+++ server/services/yodleeService.js
@@ -11,13 +11,10 @@ class YodleeService {
     }
 
     getCobSessionToken(cb) {
-        var stringBuilder = [];
-        stringBuilder.push(config.apis.yodlee.url);
-        stringBuilder.push('/authenticate/coblogin?cobrandLogin=');
-        stringBuilder.push(config.apis.yodlee.credentials.username);
-        stringBuilder.push('&cobrandPassword=');
-        stringBuilder.push(config.apis.yodlee.credentials.password);
-        var reqUrl = stringBuilder.join('');
+        var reqUrl = '';
+        reqUrl += config.apis.yodlee.url;
+        reqUrl += '/authenticate/coblogin?cobrandLogin=' + config.apis.yodlee.credentials.username;
+        reqUrl += '&cobrandPassword=' + config.apis.yodlee.credentials.password;
 
         log.info('Request looks like',reqUrl);
 
@@ -42,10 +39,85 @@ class YodleeService {
         });
     }
 
-    userLogout() {
-        
+    userLogout(cobToken, userToken, cb) {
+       var reqUrl = '';
+        reqUrl += config.apis.yodlee.url;
+        reqUrl += '/jsonsdk/Login/logout?cobSessionToken=';
+        reqUrl += cobToken;
+        reqUrl += '&userSessionToken=';
+        reqUrl += userToken;
+
+        request.post({
+            url: reqUrl.join('')
+        }, function(err, res, body) {
+            cb(err, JSON.parse(res.body), body);
+        });
     }
 
+    isValidUser(cobToken, username, cb) {
+        var reqUrl = '';
+        reqUrl += config.apis.yodlee.url;
+        reqUrl += '/jsonsdk/Login/validateUser?cobSessionToken=' + cobToken;
+        reqUrl += '&userName=' + username;
+
+        request.post({
+            url: reqUrl.join('')
+        }, function(err, res, body) {
+            cb(err, JSON.parse(res.body), body);
+        });
+    }
+
+
+    /**
+     * let's get our user's data, also priming the cache on the
+     * server, with transactions from transactionSearchRequest.higherFetchLimit
+     * to transactionSearchRequest.lowerFetchLimit for accessing chunks
+     * of transactions data at a later time using this.getUserTransactions()
+     * */
+    getUserResults(cobToken, userToken, options) {
+        var reqUrl = config.apis.yodlee.url + '/jsonsdk/TransactionSearchService/executeUserSearchRequest';
+        reqUrl += '?cobSessionToken=' + cobToken;
+        reqUrl += '&userSessionToken' + userToken;
+
+
+        request.post({
+            url: reqUrl,
+            form: {
+                transactionSearchRequest: {
+                    containerType: 'ALL',
+                    higherFetchLimit: config.apis.yodlee.higherFetchLimit,
+                    lowerFetchLimit: config.apis.yodlee.lowerFetchLimit,
+                    resultRange: {
+                        endNumber: 100,
+                        startNumber: 1
+                    },
+                    searchClients: {
+                        clientId: config.apis.yodlee.searchClients.clientId,
+                        clientName: config.apis.yodlee.searchClients.clientName
+                    },
+                    ignoreUserInput: true,
+                    searchFilter: {
+                        currencyCode: options.searchFilter.currencyCode,
+                        transactionSplitType: 'P'
+                    }
+                }
+            }
+        }, function(err, req, body ) {
+           ch(err, JSON.parse(res.body), body);
+        });
+
+    }
+
+    /**
+     * let's use searchIdentifier.identifier from getUserResults
+     * to get chucks of transaction data for our user.  This request
+     * will receive chunks, from the cache on the Yodlee server, of size
+     * specified by transactionSearchRequest.resultRange.endNumber and
+     * transactionSearchRequest.resultRange.startNumber.
+     * */
+    getUserTransactions() {
+        //TODO: implement getUserTransactions
+    }
 }
 
 module.exports = YodleeService;
